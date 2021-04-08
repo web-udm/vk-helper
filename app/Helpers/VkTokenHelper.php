ed1df8bd04dd327fc254c82573e5ed214bb4479c
Ildar Mosnabeev <web.udm@gmail.com>
2021-04-08 17:11:11 +0400

    добавил валидатор ссылок и хэлпер, проверяющий наличие токена в сессии

--- app/Helpers/VkTokenHelper.php
+++ app/Helpers/VkTokenHelper.php
@@ -1,10 +1,13 @@
 <?php
 
-
 namespace App\Helpers;
 
+use App\Exceptions\TokenDiedException;
 
 class VkTokenHelper
 {
-
+    static public function isTokenInSession()
+    {
+        session()->has('vk_token') ? true : throw new TokenDiedException();
+    }
 }
--- app/Validators/LinksValidator.php
+++ app/Validators/LinksValidator.php
@@ -1,10 +1,26 @@
 <?php
 
-
 namespace App\Validators;
 
+use App\Exceptions\EmptyLinksListException;
+use App\Exceptions\InvalidLinkException;
 
 class LinksValidator
 {
+    public function validate(array $links): bool
+    {
+        if (empty($links[0])) {
+            throw new EmptyLinksListException();
+        }
 
+        for ($i = 0; $i < count($links); $i++) {
+            $reg = '#https://vk.com/.*#';
+
+            if (preg_match($reg, $links[$i]) === 0) {
+                throw new InvalidLinkException(linkNumber: $i + 1);
+            }
+        }
+
+        return true;
+    }
 }
