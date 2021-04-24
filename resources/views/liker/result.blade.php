13eae834f8d60ea092fdb4809458d2f72b349808
Ildar Mosnabeev <web.udm@gmail.com>
2021-04-24 19:06:40 +0400

    сделал представление для результатов лайкинга

--- resources/views/liker/result.blade.php
+++ resources/views/liker/result.blade.php
@@ -3,5 +3,22 @@
 @section('title', 'Результаты лайкинга')
 
 @section('content')
-    {{ $likingTasks }}
+    <h1> Задачи по лайкингу: </h1>
+    <ul class="liking-tasks__list">
+        @foreach ($likingTasks as $likingTask)
+            <div class="liking-task__item-wrapper {{ $likingTask->status == 'Успешно завершено'
+                                                    ? 'liking-task__item-wrapper__complete'
+                                                    : 'liking-task__item-wrapper__in-process' }}">
+                <div class="liking-task__item-parameter">
+                    {{ $likingTask->id }}
+                </div>
+                <div class="liking-task__item-parameter">
+                    {{ $likingTask->status }}
+                </div>
+                <div class="liking-task__item-parameter">
+                    {{ $likingTask->date }}
+                </div>
+            </div>
+        @endforeach
+    </ul>
 @endsection
